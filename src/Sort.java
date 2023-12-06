1a94a2db3a6a2156b472c9d375c8853301b0f43c
Lukmanudhin M <lukmanudhinm@gmail.com>
2023-12-06 21:12:16 +0400

    fix c style array declaration

--- src/Sort.java
+++ src/Sort.java
@@ -1,7 +1,7 @@
 public class Sort {
 
     public static void main(String[] args) {
-        int num[]={8,6,5,4,9,1,3,5,2,0};
+        int[] num ={8,6,5,4,9,1,3,5,2,0};
         int temp;
         for (int i= 0;i<num.length;i++){
             for (int j=i+1;j< num.length;j++){
